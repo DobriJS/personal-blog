b34f8eb52498198d5064ab890ce8a02bf9a2d1a9
DobriJS <53184157+DobriJS@users.noreply.github.com>
2022-06-21 13:47:06 +0300

    Add Main Navigation styles

--- components/Layout/MainNavigation.js
+++ components/Layout/MainNavigation.js
@@ -1,8 +1,9 @@
 import Link from 'next/link';
+import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
   return (
-    <header>
+    <header className={classes.header}>
       <Link href='/'>
         <a></a>
       </Link>

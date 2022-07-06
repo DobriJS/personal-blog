fcd2b063738915b9b911d9f7f4fc1f68cb49d0c2
DobriJS <53184157+DobriJS@users.noreply.github.com>
2022-07-07 02:14:36 +0300

    Rendering Images With Image Component

--- components/Posts/PostDetail/PostContent.js
+++ components/Posts/PostDetail/PostContent.js
@@ -1,14 +1,38 @@
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 import PostHeader from './PostHeader';
 import classes from './PostContent.module.css';
 
 const PostContent = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    }
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
